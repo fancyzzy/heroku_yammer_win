24d088990641155f9e18fd5799dc59c808a374be
fancyzzy <fancyzzy@163.com>
2018-10-25 16:40:06 +0800

    bug jquery refresh

--- yammer_rank/main/views.py
+++ yammer_rank/main/views.py
@@ -10,6 +10,7 @@ from flask import request
 from flask import render_template
 from flask import session
 from flask import redirect, url_for
+from flask import jsonify
 
 from datetime import datetime
 import time
@@ -130,6 +131,7 @@ def yammer_rank():
 @main.route('/rank_result', methods=['POST', 'GET'])
 def get_rank_result():
 
+    print("DEBUG this is get_rank_result view fuction")
     end_date = None
     start_date = None
     letter_num = 1
@@ -254,6 +256,137 @@ def get_rank_result():
         rank_category = "Post"
 
     return render_template('main/rank_result.html', mylist=yammer_result, img_data=img_data, rank_category=rank_category)
+
+
+# celery
+@main.route('/long_task', methods=['POST'])
+def long_task():
+    print("DEBUG this is long_task called from  ajax!!!!")
+    end_date = None
+    start_date = None
+    letter_num = 1
+    least_comment_num = 1
+    final_comment_num = 50
+    show_top = 10
+    group_id = '15273590'
+    rank_for_post = False
+    # The final rankings
+    yammer_result = None
+    total_comments = 0
+    total_posts = 0
+    access_token = session["access_token"]
+    if not access_token:
+        print("login expired, need to login first")
+        return "Login needed"
+
+    if request.method == 'POST':
+        # yammer_result = ["a", "b", "c"]
+        # str_now = datetime.now().strftime("%Y/%m/%d")
+        end_date = request.form['end_date']
+        start_date = request.form['start_date']
+        letter_num = request.form['letter_num']
+        least_comment_num = request.form['least_comment_num']
+        final_comment_num = request.form['final_comment_num']
+        show_top = request.form['show_top']
+        group_id = request.form['sel_group']
+        # check group id validation
+        print("DEBUG you input group_id :{}".format(group_id))
+
+        rank_for_post = int(request.form['rank_for_post'])
+        if rank_for_post == 0:
+            rank_for_post = False
+        else:
+            rank_for_post = True
+
+        if letter_num.isdigit():
+            letter_num = int(letter_num)
+        else:
+            print("DEBUG no digit type!")
+            letter_num = 1
+
+        if least_comment_num.isdigit():
+            least_comment_num = int(least_comment_num)
+        else:
+            least_comment_num = 1
+
+        if start_date == "":
+            start_date = None
+        else:
+            start_date = start_date.replace('-', '/')
+
+        if end_date == "":
+            end_date = None
+        else:
+            end_date = end_date.replace('-', '/')
+
+    if request.method == 'GET':
+        print("DEBUG GET rank_result")
+        end_date = None
+        start_date = None
+        letter_num = 1
+        least_comment_num = 1
+        final_comment_num = 50
+        show_top = 10
+
+    # Get the group messages and users
+    # if there is in db, then direct get data from db
+    # if there is not fresh data in db, then start the crawler
+
+    # group_id = 15273590
+    # group_id = 12562314
+    ya = my_yammer.My_Yammer(access_token)
+    group_name = ya.pull_group_name(group_id)
+    if group_name == None:
+        # wrong group id
+        # alert(invalid group id)
+        return "Invalid group id"
+    else:
+
+        # if new user, refresh the db user info
+        # pass
+
+        if yammer_result == None:
+            yammer_result, total_comments, total_posts = ya.get_group_rank(group_id, letter_num, least_comment_num, \
+                                                                           end_date, start_date, rank_for_post)
+        # use celery
+        '''
+        print("DEBUG Start to call the long_time_task va celery")
+        yammer_result = long_time_task.delay(ya, group_id, letter_num, least_comment_num, \
+                                       end_date, start_date, rank_for_post)
+        '''
+
+    if yammer_result == None:
+        print("DEBUG None message data, perhaps wrong group id!")
+
+    # return render_template('yammer_rank.html', mylist=yammer_result, img_name=img_url)
+    print("DEBUG start to created png")
+    # 转成图片的步骤
+    # tkinter RuntimeError: main thread is not in main loop
+    # here need another thread?
+    plt = my_plot.draw_figure(yammer_result, 0, end_date, start_date, final_comment_num, show_top, group_name, \
+                              total_comments, total_posts)
+    print("DEBUG Get plt id: {}".format(id(plt)))
+
+    if start_date == None:
+        start_date = "the ever biggning"
+    if end_date == None:
+        end_date = "now"
+
+    from io import BytesIO
+    import base64
+
+    sio = BytesIO()
+    plt.savefig(sio, format='png', dpi=100)
+    img_data = base64.b64encode(sio.getvalue()).decode()
+    # plt.close()
+    rank_category = "Comment"
+    if rank_for_post:
+        rank_category = "Post"
+
+    response = {'yammer_result':yammer_result, 'img_data':img_data}
+
+    return jsonify(response), 200
+
 
 
 if __name__ == '__main__':
